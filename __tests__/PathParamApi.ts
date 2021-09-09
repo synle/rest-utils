8a335023a989678efd37dc6748e9bae809412b36
Sy Le <lenguyensy@gmail.com>
2021-09-09 10:02:39 -0700

    Update .prettier Format config (#5)
    * code format
    
    * 1.0.2

--- __tests__/PathParamApi.ts
+++ __tests__/PathParamApi.ts
@@ -1,16 +1,11 @@
-import { makeRestApi, RestApiResponse } from "synle-rest-utils";
+import { makeRestApi, RestApiResponse } from 'synle-rest-utils';
 
 interface CookieResponse {
   cookies: Record<string | number, string | number>;
 }
 
 export class PathParamApi {
-  static setCookie(
-    cookieName: string,
-    cookieValue: string
-  ): RestApiResponse<CookieResponse> {
-    return makeRestApi(
-      `https://httpbin.org/cookies/set?${cookieName}=${cookieValue}`
-    );
+  static setCookie(cookieName: string, cookieValue: string): RestApiResponse<CookieResponse> {
+    return makeRestApi(`https://httpbin.org/cookies/set?${cookieName}=${cookieValue}`);
   }
 }
